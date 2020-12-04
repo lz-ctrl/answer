c78f2ad6c9689871f5255b5eafde934a7a87e095
lz <312462719@qq.com>
2020-12-04 11:56:51 +0800

    d

---
+++ src/main/java/com/answer/api/AnswerApplication.java
@@ -0,0 +1,16 @@
+package com.answer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lz
+ */
+@SpringBootApplication
+public class AnswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnswerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/answer/api/AnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.answer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

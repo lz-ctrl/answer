0d517ff9da1b10995cba188b1f14377aeb9741be
yjj <674039309@qq.com>
2020-12-18 10:18:17 +0800

    yjj202012181217

--- src/main/java/com/answer/api/entity/CharacterAnalysis.java
+++ src/main/java/com/answer/api/entity/CharacterAnalysis.java
@@ -1,8 +1,24 @@
 package com.answer.api.entity;
 
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
+/**
+ * @author YinJunjie
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("tb_character_analysis")
 public class CharacterAnalysis implements Serializable {
+
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     private String characterName;
@@ -17,61 +33,4 @@ public class CharacterAnalysis implements Serializable {
 
     private String workStyle;
 
-    private static final long serialVersionUID = 1L;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCharacterName() {
-        return characterName;
-    }
-
-    public void setCharacterName(String characterName) {
-        this.characterName = characterName == null ? null : characterName.trim();
-    }
-
-    public String getPersonalityProfile() {
-        return personalityProfile;
-    }
-
-    public void setPersonalityProfile(String personalityProfile) {
-        this.personalityProfile = personalityProfile == null ? null : personalityProfile.trim();
-    }
-
-    public String getPersonalityCharacteristics() {
-        return personalityCharacteristics;
-    }
-
-    public void setPersonalityCharacteristics(String personalityCharacteristics) {
-        this.personalityCharacteristics = personalityCharacteristics == null ? null : personalityCharacteristics.trim();
-    }
-
-    public String getAdvantage() {
-        return advantage;
-    }
-
-    public void setAdvantage(String advantage) {
-        this.advantage = advantage == null ? null : advantage.trim();
-    }
-
-    public String getShortcoming() {
-        return shortcoming;
-    }
-
-    public void setShortcoming(String shortcoming) {
-        this.shortcoming = shortcoming == null ? null : shortcoming.trim();
-    }
-
-    public String getWorkStyle() {
-        return workStyle;
-    }
-
-    public void setWorkStyle(String workStyle) {
-        this.workStyle = workStyle == null ? null : workStyle.trim();
-    }
 }
